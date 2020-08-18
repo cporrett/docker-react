26fab0df9be2637d8ceee45fe94acd5adacb504b
cporrett <jockstein292@yahoo.co.uk>
2020-08-18 16:57:30 +0200

    Changed app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p className="App-intro">
-          Hello Craig modified again?
+          Changed to feature branch?
         </p>
       </header>
     </div>
