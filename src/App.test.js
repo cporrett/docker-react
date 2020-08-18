7016a2b0a6cf6b514221a5bc87b26c663b967491
cporrett <jockstein292@yahoo.co.uk>
2020-08-18 14:39:49 +0200

    inital commit

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello Craig/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+it('additional test', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Hello Craig/i);
+  expect(linkElement).toBeInTheDocument();
+});
